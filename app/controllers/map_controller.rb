5c4006b6147b848376e2ec4398e500d0796c78b2
geeq <geeq@d795b529-0e00-4a15-92ba-4f9d98aa6e02>
2008-03-03 02:13:15 +0000

    google maps api plugin in
    git-svn-id: https://svn.72dpiarmy.beanstalkapp.com/urbantakeover/trunk@10 d795b529-0e00-4a15-92ba-4f9d98aa6e02

---
+++ app/controllers/map_controller.rb
@@ -0,0 +1,12 @@
+class MapController < ApplicationController
+
+  def index
+    @map = GMap.new("map_div_id")  
+    @map.control_init(:large_map => true, :map_type => true)  
+    @map.center_zoom_init([75.5,-42.56], 4)  
+
+    marker = GMarker.new([75.6, -42.467],   
+      :title => "Where Am I?", :info_window => "Hello, Greenland")  
+    @map.overlay_init(marker)
+  end
+end
