1aad4a7a69cd7e942f898d099878eb2a91d06bbc
geeq <geeq@d795b529-0e00-4a15-92ba-4f9d98aa6e02>
2008-03-06 21:37:52 +0000

    fuck
    git-svn-id: https://svn.72dpiarmy.beanstalkapp.com/urbantakeover/trunk@33 d795b529-0e00-4a15-92ba-4f9d98aa6e02

---
+++ app/helpers/claims_helper.rb
@@ -0,0 +1,19 @@
+module ClaimsHelper
+  def sort_claims_to_days claims
+    days = []
+    day = claims[0].created_at.day
+    claims_at_day = []
+    claims.each do |claim|
+      if day != claim.created_at.day
+        days << claims_at_day
+        claims_at_day = [claim]
+        day = claim.created_at.day
+      else
+        claims_at_day << claim
+      end
+    end
+
+    days << claims_at_day
+    days
+  end
+end
