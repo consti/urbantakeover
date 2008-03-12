6419ee3fc3c527ea51ee3e14ad3323e0fae37eed
geeq <geeq@d795b529-0e00-4a15-92ba-4f9d98aa6e02>
2008-03-12 18:11:53 +0000

    oops, totally forgot that controller
    git-svn-id: https://svn.72dpiarmy.beanstalkapp.com/urbantakeover/trunk@68 d795b529-0e00-4a15-92ba-4f9d98aa6e02

---
+++ app/controllers/gateway_controller.rb
@@ -0,0 +1,49 @@
+class GatewayController < ApplicationController
+  before_filter :login_required, :only => :website
+
+  def self.private_api values
+    before_filter :verify_private_api, :only => values
+    skip_before_filter :verify_authenticity_token, :only => values
+  end
+  
+  private_api :twitter
+
+  def twitter
+    render :text => handle_message(params[:user], params[:message])
+  end
+  
+  def website
+    command = Command.create(:user => current_user, :text => params[:command][:text])
+    result = command.run!
+    flash[:notice] = result || "sorry, something went wrong. no result text??? o_O'"
+    return redirect_back_or_default root_url
+  end
+
+private
+  def verify_private_api
+    render :text => "gotcha! sorry, but you needs special powarz!" unless params[:key] == PRIVATE_API_KEY
+  end
+
+  def handle_message username, message
+    message.downcase!
+    #username = username.downcase!
+
+    return "no username" if (username == nil or username.empty?)
+
+    return_message = ""
+    user = User.find_by_twittername username
+    unless user
+      password = "%04d" % (1+rand(9999))
+      user = User.create(:twittername => username,
+                         :login => username,
+                         :password => password,
+                         :password_confirmation => password)
+      return_message += "Ohay #{username} your password is #{password}! "
+      user.save!
+      user.reload
+    end
+    
+    command = Command.create(:user => user, :text => message)
+    return return_message + command.run!
+  end
+end
