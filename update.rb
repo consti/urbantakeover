27192350ead9f061f88abeb038a680735597d3f7
Florian Hufsky <flo@oneup.local>
2008-03-31 17:40:01 +0200

    lazy mans flickr integration

--- update.rb
+++ update.rb
@@ -1,4 +1,4 @@
-#restart the server
+#restart the server 
 
 `/home/consti/opt/bin/git pull origin master`
 
@@ -9,7 +9,9 @@
   end
 end
 
-`kill #{pid}` if pid`
+`kill #{pid}` if pid
 
 `export GEM_PATH=~/gems`
 `./autostart.cgi`
+
+print "now start script/console production and send an update message via TWITTER.post 'i fixded this'"
