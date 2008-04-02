36ca4ff4935090896b86605ecfacce006d35aa75
Florian Hufsky <flo@oneup.local>
2008-04-02 17:48:48 +0200

    -_-

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -4,7 +4,7 @@ class TeamController < ApplicationController
   end
   
   def show_by_name
-    @user = Team.find_by_name params[:name]
+    @team = Team.find_by_name params[:name]
     render :template => 'team/show'
   end
 end
