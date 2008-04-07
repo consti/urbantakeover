02242ebd3a380d7d2f8213ab31e25666cd5c4bf8
Florian Hufsky <flo@oneup.local>
2008-04-07 16:56:25 +0200

    GUI fixes

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,10 +1,14 @@
 class TeamController < ApplicationController
   def show
     @team = Team.find params[:id]
+    @team ||= Team.find_by_name params[:id]
   end
   
   def show_by_name
     @team = Team.find_by_name params[:name]
     render :template => 'team/show'
+  end
+  
+  def index
   end
 end
