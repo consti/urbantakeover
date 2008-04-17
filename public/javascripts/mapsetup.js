b49847b024700fb1dff012417f6a268d67a29188
Consti <Consti@Consti.de>
2008-04-17 19:07:01 +0200

    added 'show in flickr uto group' linkt o spots info window

--- public/javascripts/mapsetup.js
+++ public/javascripts/mapsetup.js
@@ -93,7 +93,7 @@ function newMarker(markerLocation, spotId, addr, users, mrkclr, baseIcon, select
 		for(var i=1;i<users.length; i++) {
 			infoMsg+=', <a href\"/user/'+users[i][0]+'" class=\"user-name\" style=\"background-color:'+users[i][1]+';border-color:'+users[i][2]+';\">'+users[i][0]+'</a>';
 		}
-		infoMsg+='<br/><a href=\"http://flickr.com/photos/tags/'+spotId+'\">Tag on flickr</a>';
+		infoMsg+='<br/><a href=\"http://flickr.com/photos/tags/'+spotId+'\">Tag on flickr</a> & <a href=\"http://flickr.com/search/groups/?q='+spotId+'&w=697289%40N21&m=pool\">in uto group</a>';
 	
 	GEvent.addListener(marker, 'click', function() {
 		marker.openInfoWindowHtml(infoMsg);
