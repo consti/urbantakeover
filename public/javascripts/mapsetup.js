f498f2500c0d37d146f2b95c11d8f63a1ccab4f5
Consti <Consti@Consti.de>
2008-04-22 03:37:11 +0200

    foo4

--- public/javascripts/mapsetup.js
+++ public/javascripts/mapsetup.js
@@ -168,10 +168,6 @@ function mapini() {
 			} else if (json[i][2][0]) {
 				marker=newMarker(new GLatLng(json[i][4], json[i][5]), json[i][0], json[i][1], json[i][2], json[i][3], baseIcon, false);
 			}
-			    if(json[i][2][0][2] == undefined) {
-			      farbe = json[i][2][0][2].replace(/#/,"");
-            map.addOverlay(new EInsert(new GLatLng(json[i][4], json[i][5]), "/images/areas/"+farbe+".png", new GSize(0.0005,0.0005), -1));
-			    }  
 				markersArray.push(marker);
 		}
 
